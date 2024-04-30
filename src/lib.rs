df94f6400f15d0bbc592326290731955f3572fd3
maxall41 <maxall4@protonmail.com>
2024-04-30 14:53:26 -0700

    Output polar & non polar SASA values

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,18 @@
 //! RustSASA is a Rust library for computing the absolute solvent accessible surface area (ASA/SASA) of each atom in a given protein structure using the Shrake-Rupley algorithm[1].
-mod utils;
+mod consts;
 mod test;
+mod utils;
 
-use std::collections::HashMap;
+use crate::consts::POLAR_AMINO_ACIDS;
+use crate::utils::{serialize_chain_id, simd_sum};
 use nalgebra::{Point3, Vector3};
-use pdbtbx::{PDB};
+use pdbtbx::PDB;
 use rayon::prelude::*;
 use rstar::{PointDistance, RTree, RTreeObject, AABB};
-use std::sync::Arc;
-use snafu::{OptionExt};
 use snafu::prelude::*;
-use crate::utils::{serialize_chain_id, simd_sum};
+use snafu::OptionExt;
+use std::collections::HashMap;
+use std::sync::Arc;
 
 /// This struct represents an individual Atom
 #[derive(Clone)]
@@ -22,7 +24,7 @@ pub struct Atom {
     /// A unique Id for the atom
     pub id: usize,
     /// Parent Id
-    pub parent_id: Option<isize>
+    pub parent_id: Option<isize>,
 }
 
 /// Can be used to specify output resolution of SASA computation for convenience.
@@ -33,24 +35,36 @@ pub enum SASALevel {
     Protein,
 }
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct ChainResult {
     pub name: String,
-    pub value: f32
+    pub value: f32,
 }
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct ResidueResult {
     pub serial_number: isize,
-    pub value: f32
+    pub value: f32,
+    pub name: String,
+    pub is_polar: bool,
 }
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
+pub struct ProteinResult {
+    /// The total SASA value for the entire protein
+    pub global_total: f32,
+    /// The total polar SASA value for the entire protein
+    pub polar_total: f32,
+    /// The total *non*-polar SASA value for the entire protein
+    pub non_polar_total: f32,
+}
+
+#[derive(Debug, PartialEq)]
 pub enum SASAResult {
     Atom(Vec<f32>),
     Residue(Vec<ResidueResult>),
     Chain(Vec<ChainResult>),
-    Protein(f32)
+    Protein(ProteinResult),
 }
 
 #[derive(Debug, Snafu)]
@@ -63,8 +77,10 @@ pub enum SASACalcError {
 
     #[snafu(display("Failed to map atoms back to level element"))]
     AtomMapToLevelElementFailed,
-}
 
+    #[snafu(display("Failed to get residue name"))]
+    FailedToGetResidueName,
+}
 
 impl RTreeObject for Atom {
     type Envelope = AABB<[f32; 3]>;
@@ -211,35 +227,59 @@ pub fn calculate_sasa_internal(
 /// ).unwrap();
 /// let result = calculate_sasa(&pdb,None,None,SASALevel::Residue);
 /// ```
-pub fn calculate_sasa(pdb: &PDB,probe_radius: Option<f32>, n_points: Option<usize>,level: SASALevel) -> Result<SASAResult, SASACalcError> {
+pub fn calculate_sasa(
+    pdb: &PDB,
+    probe_radius: Option<f32>,
+    n_points: Option<usize>,
+    level: SASALevel,
+) -> Result<SASAResult, SASACalcError> {
     let mut atoms = vec![];
     let mut parent_to_atoms = HashMap::new();
     match level {
-        SASALevel::Atom | SASALevel::Protein => {
+        SASALevel::Atom => {
             for atom in pdb.atoms() {
                 atoms.push(Atom {
-                    position: Point3::new(atom.pos().0 as f32, atom.pos().1 as f32, atom.pos().2 as f32),
-                    radius: atom.element().context(ElementMissingSnafu)?.atomic_radius().van_der_waals.context(VanDerWaalsMissingSnafu)? as f32,
+                    position: Point3::new(
+                        atom.pos().0 as f32,
+                        atom.pos().1 as f32,
+                        atom.pos().2 as f32,
+                    ),
+                    radius: atom
+                        .element()
+                        .context(ElementMissingSnafu)?
+                        .atomic_radius()
+                        .van_der_waals
+                        .context(VanDerWaalsMissingSnafu)? as f32,
                     id: atom.serial_number(),
-                    parent_id: None
+                    parent_id: None,
                 })
             }
         }
-        SASALevel::Residue => {
+        SASALevel::Residue | SASALevel::Protein => {
             let mut i = 0;
             for residue in pdb.residues() {
                 let mut temp = vec![];
                 for atom in residue.atoms() {
                     atoms.push(Atom {
-                        position: Point3::new(atom.pos().0 as f32, atom.pos().1 as f32, atom.pos().2 as f32),
-                        radius: atom.element().context(ElementMissingSnafu)?.atomic_radius().van_der_waals.context(VanDerWaalsMissingSnafu)? as f32,
+                        position: Point3::new(
+                            atom.pos().0 as f32,
+                            atom.pos().1 as f32,
+                            atom.pos().2 as f32,
+                        ),
+                        radius: atom
+                            .element()
+                            .context(ElementMissingSnafu)?
+                            .atomic_radius()
+                            .van_der_waals
+                            .context(VanDerWaalsMissingSnafu)?
+                            as f32,
                         id: atom.serial_number(),
-                        parent_id: Some(residue.serial_number())
+                        parent_id: Some(residue.serial_number()),
                     });
                     temp.push(i);
                     i += 1;
                 }
-                parent_to_atoms.insert(residue.serial_number(),temp);
+                parent_to_atoms.insert(residue.serial_number(), temp);
             }
         }
         SASALevel::Chain => {
@@ -249,29 +289,42 @@ pub fn calculate_sasa(pdb: &PDB,probe_radius: Option<f32>, n_points: Option<usiz
                 let chain_id = serialize_chain_id(chain.id());
                 for atom in chain.atoms() {
                     atoms.push(Atom {
-                        position: Point3::new(atom.pos().0 as f32, atom.pos().1 as f32, atom.pos().2 as f32),
-                        radius: atom.element().context(ElementMissingSnafu)?.atomic_radius().van_der_waals.context(VanDerWaalsMissingSnafu)? as f32,
+                        position: Point3::new(
+                            atom.pos().0 as f32,
+                            atom.pos().1 as f32,
+                            atom.pos().2 as f32,
+                        ),
+                        radius: atom
+                            .element()
+                            .context(ElementMissingSnafu)?
+                            .atomic_radius()
+                            .van_der_waals
+                            .context(VanDerWaalsMissingSnafu)?
+                            as f32,
                         id: atom.serial_number(),
-                        parent_id: Some(chain_id)
+                        parent_id: Some(chain_id),
                     });
                     temp.push(i);
                     i += 1
                 }
-                parent_to_atoms.insert(chain_id,temp);
+                parent_to_atoms.insert(chain_id, temp);
             }
         }
     }
     let atom_sasa = calculate_sasa_internal(&atoms, probe_radius, n_points);
     return match level {
-        SASALevel::Atom => {
-            Ok(SASAResult::Atom(atom_sasa))
-        },
+        SASALevel::Atom => Ok(SASAResult::Atom(atom_sasa)),
         SASALevel::Chain => {
             let mut chain_sasa = vec![];
             for chain in pdb.chains() {
                 let chain_id = serialize_chain_id(chain.id());
-                let chain_atom_index = parent_to_atoms.get(&chain_id).context(AtomMapToLevelElementFailedSnafu)?;
-                let chain_atoms: Vec<_> = chain_atom_index.iter().map(|&index| atom_sasa[index]).collect();
+                let chain_atom_index = parent_to_atoms
+                    .get(&chain_id)
+                    .context(AtomMapToLevelElementFailedSnafu)?;
+                let chain_atoms: Vec<_> = chain_atom_index
+                    .iter()
+                    .map(|&index| atom_sasa[index])
+                    .collect();
                 let sum = simd_sum(chain_atoms.as_slice());
                 chain_sasa.push(ChainResult {
                     name: chain.id().to_string(),
@@ -279,23 +332,59 @@ pub fn calculate_sasa(pdb: &PDB,probe_radius: Option<f32>, n_points: Option<usiz
                 })
             }
             Ok(SASAResult::Chain(chain_sasa))
-        },
+        }
         SASALevel::Residue => {
             let mut residue_sasa = vec![];
             for residue in pdb.residues() {
-                let residue_atom_index = parent_to_atoms.get(&residue.serial_number()).context(AtomMapToLevelElementFailedSnafu)?;
-                let residue_atoms: Vec<_> = residue_atom_index.iter().map(|&index| atom_sasa[index]).collect();
+                let residue_atom_index = parent_to_atoms
+                    .get(&residue.serial_number())
+                    .context(AtomMapToLevelElementFailedSnafu)?;
+                let residue_atoms: Vec<_> = residue_atom_index
+                    .iter()
+                    .map(|&index| atom_sasa[index])
+                    .collect();
                 let sum = simd_sum(residue_atoms.as_slice());
+                let name = residue
+                    .name()
+                    .context(FailedToGetResidueNameSnafu)?
+                    .to_string();
                 residue_sasa.push(ResidueResult {
                     serial_number: residue.serial_number(),
                     value: sum,
+                    is_polar: POLAR_AMINO_ACIDS.contains(&name),
+                    name,
                 })
             }
             Ok(SASAResult::Residue(residue_sasa))
         }
         SASALevel::Protein => {
-            let sum = simd_sum(atom_sasa.as_slice());
-           Ok(SASAResult::Protein(sum))
+            let mut polar_total: f32 = 0.0;
+            let mut non_polar_total: f32 = 0.0;
+            for residue in pdb.residues() {
+                let residue_atom_index = parent_to_atoms
+                    .get(&residue.serial_number())
+                    .context(AtomMapToLevelElementFailedSnafu)?;
+                let residue_atoms: Vec<_> = residue_atom_index
+                    .iter()
+                    .map(|&index| atom_sasa[index])
+                    .collect();
+                let sum = simd_sum(residue_atoms.as_slice());
+                let name = residue
+                    .name()
+                    .context(FailedToGetResidueNameSnafu)?
+                    .to_string();
+                if POLAR_AMINO_ACIDS.contains(&name) {
+                    polar_total += sum
+                } else {
+                    non_polar_total += sum
+                }
+            }
+            let global_sum = simd_sum(atom_sasa.as_slice());
+            Ok(SASAResult::Protein(ProteinResult {
+                global_total: global_sum,
+                polar_total,
+                non_polar_total,
+            }))
         }
-    }
+    };
 }
